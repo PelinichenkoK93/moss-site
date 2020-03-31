7985d3efd823b11c311206ddde7ae23121a229b7
PelinichenkoK93 <61136844+PelinichenkoK93@users.noreply.github.com>
2020-03-31 13:09:43 +0300

    first commit

---
+++ js/js.js
@@ -0,0 +1,58 @@
+$(function(){
+    $('#p1')
+    .click(function(){
+        $('#popup-container1').fadeIn(900);
+    });
+    $('#popup-container1').click(function(event){
+        if(event.target == this)
+        $(this).fadeOut(500);
+    })
+    $('#p2')
+    .click(function(){
+        $('#popup-container2').fadeIn(900);
+    });
+    $('#popup-container2').click(function(event){
+        if(event.target == this)
+        $(this).fadeOut(500);
+    })
+    $('#p3')
+    .click(function(){
+        $('#popup-container3').fadeIn(900);
+    });
+    $('#popup-container3').click(function(event){
+        if(event.target == this)
+        $(this).fadeOut(500);
+    })
+    $('#p4')
+    .click(function(){
+        $('#popup-container4').fadeIn(900);
+    });
+    $('#popup-container4').click(function(event){
+        if(event.target == this)
+        $(this).fadeOut(500);
+    })
+    $('#p5')
+    .click(function(){
+        $('#popup-container5').fadeIn(900);
+    });
+    $('#popup-container5').click(function(event){
+        if(event.target == this)
+        $(this).fadeOut(500);
+    })
+    $('#p6')
+    .click(function(){
+        $('#popup-container6').fadeIn(900);
+    });
+    $('#popup-container6').click(function(event){
+        if(event.target == this)
+        $(this).fadeOut(500);
+    })
+    $('#p7')
+    .click(function(){
+        $('#popup-container7').fadeIn(900);
+    });
+    $('#popup-container7').click(function(event){
+        if(event.target == this)
+        $(this).fadeOut(500);
+    })
+});
